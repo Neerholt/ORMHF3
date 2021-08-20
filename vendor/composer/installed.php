fe2c7c68abecac6c5a3d7455c39622f398c62a13
Victor <victorneerholt@gmail.com>
2021-08-20 10:46:53 +0200

    1.0.0.7 Added autoload files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'fa41d4bc32efaa6bb1b7f64ffb8ab904c8c5ccad',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fa41d4bc32efaa6bb1b7f64ffb8ab904c8c5ccad',
+    ),
+  ),
+);
